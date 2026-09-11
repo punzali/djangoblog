724635cbed90beb634bae0d136dfe6f0ddb8e9ed
punzali <thurmila1220@gmail.com>
2026-09-11 11:31:09 +0900

    2026.09011 about CSS

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'blog/home.html', {
+        'message': 'Hello Djangoblog'
+    })
+def about(request):
+    return render(request, 'blog/about.html', {
+        'content': 'This is the Djangoblog team.'
+    })
+def contact(request):
+    return render(request, 'blog/contact.html', {
+        'ABC': 'This is the Djangoblog Contact.'
+    })
